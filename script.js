379b44bc70e9ca6c217ee6b905ab875c2fe30bbf
kamp <lexiaky@gmail.com>
2025-11-14 17:27:01 -0500

    color button

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let buttonEl = document.getElementsByName("button")
-let textEl = 
+let buttonEl = document.getElementById("button")
+let textEl = document.getElementById("text")
 
-buttonEl.addEventListener("click", (changeColor) => {
-
+buttonEl.addEventListener("click", () => {
+    let current = getComputedStyle(textEl).color; 
+if (current === "rgb(255, 0, 0)"){textEl.style.color="blue"}
+else if (current === "rgb(0, 0, 255)"){textEl.style.color="green"}
+else if (current === "rgb(0, 128, 0)"){textEl.style.color="red"}
 }
+)
